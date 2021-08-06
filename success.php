6709575686db5c02b7c2bfc0a63ac4e320b5fb40
natbibi <natalie.wuwu@googlemail.com>
2021-08-06 12:08:43 +0100

    :sparkles: add html skeleton for success page, that also can link back to home page

---
+++ success.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Success</title>
+</head>
+<body>
+<p>Congrats on picking a strong password!</p>
+<a href="helloworld.php">Try Again!</a>
+</body>
+</html>
