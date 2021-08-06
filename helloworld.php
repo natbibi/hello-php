f98af2b5b228566cfd9b1f23566522daa1e3fd7b
natbibi <natalie.wuwu@googlemail.com>
2021-08-06 12:02:49 +0100

    :lipstick: link google fonts

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,8 @@
 <head>
     <title>PHP</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php
