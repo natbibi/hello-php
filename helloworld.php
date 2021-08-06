fcf751ec570c7fef443b5701624378f470d7638e
natbibi <natalie.wuwu@googlemail.com>
2021-08-06 12:03:15 +0100

    :lipstick: link stylesheet

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,6 @@
 <head>
     <title>PHP</title>
+    <link rel="stylesheet" href="style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
