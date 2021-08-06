5b6275ac4e4e48bb4b485c1db132e92633a16757
natbibi <natalie.wuwu@googlemail.com>
2021-08-06 15:55:49 +0100

    :sparkles: add autofocus to password input area, add more regex elseif statements

--- helloworld.php
+++ helloworld.php
@@ -5,17 +5,32 @@ $password_err = "";
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
 
-    if(strlen($password)<8) {
+    if(strlen($password)==0) {
+        $password_err = "Your password cannot be blank!";
+    }
+    elseif(strlen($password)<5) {
+        $password_err = "Your password is too short!";
+    }
+    elseif(preg_match("/\s/",$password)) {
+        $password_err = "Your password must not contain any spaces!";
+    }
+    elseif(strlen($password)<8) {
         $password_err = "Your password must contain at least 8 Characters!";
     }
     elseif(!preg_match("#[0-9]+#",$password)) {
         $password_err = "Your password must contain at least 1 Number!";
+    }
+    elseif(!preg_match("#[2-9]+#",$password)) {
+        $password_err = "Your password cannot contain the number 1, it's too easy!";
     }
     elseif(!preg_match("#[A-Z]+#",$password)) {
         $password_err = "Your password must contain at least 1 Capital Letter!";
     }
     elseif(!preg_match("#[a-z]+#",$password)) {
         $password_err = "Your password must contain at least 1 Lowercase Letter!";
+    }
+    elseif(!preg_match("/\W/",$password)) {
+        $password_err = "Your password must contain at least 1 Special Character";
     } else {
         $password_err = "Hmmm";
         header("Location: success.php");
@@ -40,7 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <label for="password">🔑
-            <input type="text" name="password" value="<?= $password ?>">
+            <input type="text" name="password" autofocus="autofocus" value="<?= $password ?>">
         </label>
         <input type="submit" name="submit" value="Submit">
     </form>
